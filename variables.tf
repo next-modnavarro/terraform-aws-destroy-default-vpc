248b133cef09473afc4c14b050f0e57f1160ebb9
Modesto Navarro <modesto.navarro2.next@bbva.com>
2020-12-10 18:14:28 +0100

    expect the full az name as we can  programmatically get the list using the terraform data source "aws_availability_zones" and therefore filter the output

--- variables.tf
+++ variables.tf
@@ -1,11 +1,11 @@
 variable "region" {
   description = "AWS Region"
   type        = string
-  default     = "us-west-2"
+  default     = "us-east-1"
 }
 
 variable "azs" {
-  description = "List of AZs to manage using only the letters, not full AZ name"
+  description = "List of AZs to manage using full AZ name"
   type        = list
-  default     = ["a", "b", "c", "d"]
+  default     = ["us-east-1a"]
 }
